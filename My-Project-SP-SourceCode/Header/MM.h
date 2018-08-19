a002efc3dfd139a22ba1561b88c568760560258c
Lavish Swarnkar <lavishswarnkar@gmail.com>
2018-08-19 21:51:24 +0530

    Rename MM.H to MM.h

--- My-Project-SP-SourceCode/Header/MM.h
+++ My-Project-SP-SourceCode/Header/MM.h
@@ -3,7 +3,7 @@ void mainmenu()
 {    	int i,j,ch=1;
 	char s[20],k='0';
 	mosaic(1);
-	hbox(2,2,79,24,"Û",10,1);
+	hbox(2,2,79,24,"Ã›",10,1);
 	lcon("welcome",4,4,14,1);
 	lcon("to",47,4,10,1);
 	lcon("the",60,4,14,1);
@@ -12,7 +12,7 @@ void mainmenu()
 	lcon("of",58,18,10,1);
 	delay(300);
 	mosaic(2);
-	hbox(2,2,79,24,"Û",1,10);
+	hbox(2,2,79,24,"Ã›",1,10);
 	lcon("smart",26,7,1,10);
 	lcon("phones",23,14,1,10);
 
@@ -21,7 +21,7 @@ void mainmenu()
 	{    	fin.getline(s,80,'p');
 		for(j=0;s[j]!=NULL;j++)
 		{    	if(s[j]=='l')
-				s[j]='Û';
+				s[j]='Ã›';
 			if(s[j]=='k')
 				s[j]=char(223);
 			if(s[j]=='u')
@@ -41,7 +41,7 @@ void mainmenu()
 	{    	fin.getline(s,80,'p');
 		for(j=0;s[j]!=NULL;j++)
 		{     	if(s[j]=='l')
-				s[j]='Û';
+				s[j]='Ã›';
 			if(s[j]=='k')
 				s[j]=char(223);
 			if(s[j]=='u')
@@ -59,13 +59,13 @@ void mainmenu()
 	{     	fin.getline(s,80,'p');
 		for(j=0;s[j]!=NULL;j++)
 		{     	if(s[j]=='Z')
-				s[j]='Û';
+				s[j]='Ã›';
 			if(s[j]=='K')
 				s[j]=char(223);
 			if(s[j]=='N')
 				s[j]=char(222);
 			if(s[j]=='l')
-				s[j]='Û';
+				s[j]='Ã›';
 			if(s[j]=='k')
 				s[j]=char(223);
 			if(s[j]=='u')
@@ -81,23 +81,23 @@ void mainmenu()
 	int f=0;
 	delay(300);
 	mosaic(1);
-	hbox(2,2,79,24,"Û",14,1);
-	fbox(3,3,78,10,"Û",6,1);
-	hbox(2,2,79,10,"Û",14,1);
+	hbox(2,2,79,24,"Ã›",14,1);
+	fbox(3,3,78,10,"Ã›",6,1);
+	hbox(2,2,79,10,"Ã›",14,1);
 	lcon("main",15,4,10,6);
 	lcon("menu",44,4,10,6);
-	fbox(11,3,43,23,"Û",3,1);
-	fbox(11,44,78,23,"Û",2,1);
+	fbox(11,3,43,23,"Ã›",3,1);
+	fbox(11,44,78,23,"Ã›",2,1);
 	textcolor(1); textbackground(3);
-	gotoxy(4,12); cprintf("Û SMART PHONES BRIEF INFORMATION");
+	gotoxy(4,12); cprintf("Ã› SMART PHONES BRIEF INFORMATION");
 	textcolor(11); textbackground(1);
-	gotoxy(4,12); cprintf("Û");
+	gotoxy(4,12); cprintf("Ã›");
 	textcolor(1); textbackground(3);
-	gotoxy(4,14); cprintf("Û SOME POPULAR SMART PHONES DESCRIPTION");
-	gotoxy(4,16); cprintf("Û SMART PHONES FILTERING ");
-	gotoxy(4,18); cprintf("Û ADD / MODIFY / EXPORT DATA ");
-	gotoxy(4,20); cprintf("Û STATUS");
-	gotoxy(4,22); cprintf("Û AMAZE ME");
+	gotoxy(4,14); cprintf("Ã› SOME POPULAR SMART PHONES DESCRIPTION");
+	gotoxy(4,16); cprintf("Ã› SMART PHONES FILTERING ");
+	gotoxy(4,18); cprintf("Ã› ADD / MODIFY / EXPORT DATA ");
+	gotoxy(4,20); cprintf("Ã› STATUS");
+	gotoxy(4,22); cprintf("Ã› AMAZE ME");
 	textbackground(2);
 	gotoxy(45,12); cprintf(">> USE ARROW KEYS OR");
 	gotoxy(45,13); cprintf("   W/A/S/D FOR SELECTION");
@@ -136,23 +136,23 @@ f=1;
 		{       about();f=1;  }
 
 		if(f)
-		{ 	hbox(2,2,79,24,"Û",14,1,0);
-			fbox(3,3,78,10,"Û",6,1,0);
-			hbox(2,2,79,10,"Û",14,1,0);
+		{ 	hbox(2,2,79,24,"Ã›",14,1,0);
+			fbox(3,3,78,10,"Ã›",6,1,0);
+			hbox(2,2,79,10,"Ã›",14,1,0);
 			lcon("main",15,4,10,6);
 			lcon("menu",44,4,10,6);
-			fbox(11,3,43,23,"Û",3,1,0);
-			fbox(11,44,78,23,"Û",2,1,0);
+			fbox(11,3,43,23,"Ã›",3,1,0);
+			fbox(11,44,78,23,"Ã›",2,1,0);
 			textcolor(1); textbackground(3);
-			gotoxy(4,12); cprintf("Û SMART PHONES BRIEF INFORMATION");
+			gotoxy(4,12); cprintf("Ã› SMART PHONES BRIEF INFORMATION");
 			textcolor(11); textbackground(1);
-			gotoxy(4,12); cprintf("Û");
+			gotoxy(4,12); cprintf("Ã›");
 			textcolor(1); textbackground(3);
-			gotoxy(4,14); cprintf("Û SOME POPULAR SMART PHONES DESCRIPTION");
-			gotoxy(4,16); cprintf("Û SMART PHONES FILTERING ");
-			gotoxy(4,18); cprintf("Û ADD / MODIFY /EXPORT DATA ");
-			gotoxy(4,20); cprintf("Û STATUS");
-			gotoxy(4,22); cprintf("Û AMAZE ME");
+			gotoxy(4,14); cprintf("Ã› SOME POPULAR SMART PHONES DESCRIPTION");
+			gotoxy(4,16); cprintf("Ã› SMART PHONES FILTERING ");
+			gotoxy(4,18); cprintf("Ã› ADD / MODIFY /EXPORT DATA ");
+			gotoxy(4,20); cprintf("Ã› STATUS");
+			gotoxy(4,22); cprintf("Ã› AMAZE ME");
 			textbackground(2);
 			gotoxy(45,12); cprintf(">> USE ARROW KEYS OR");
 			gotoxy(45,13); cprintf("   W/A/S/D FOR SELECTION");
@@ -166,37 +166,37 @@ f=1;
 				{	gotoxy(4,12+(2*i));
 					if(i==0) textcolor(11);
 					else     textcolor(1);
-					cprintf("Û");
+					cprintf("Ã›");
 				} break;
 			case 2: for(i=0;i<=5;i++)
 				{	gotoxy(4,12+(2*i));
 					if(i==1) textcolor(11);
 					else     textcolor(1);
-					cprintf("Û");
+					cprintf("Ã›");
 				} break;
 			case 3: for(i=0;i<=5;i++)
 				{	gotoxy(4,12+(2*i));
 					if(i==2) textcolor(11);
 					else     textcolor(1);
-					cprintf("Û");
+					cprintf("Ã›");
 				} break;
 			case 4: for(i=0;i<=5;i++)
 				{	gotoxy(4,12+(2*i));
 					if(i==3) textcolor(11);
 					else     textcolor(1);
-					cprintf("Û");
+					cprintf("Ã›");
 				} break;
 			case 5: for(i=0;i<=5;i++)
 				{	gotoxy(4,12+(2*i));
 					if(i==4) textcolor(11);
 					else     textcolor(1);
-					cprintf("Û");
+					cprintf("Ã›");
 				} break;
 			case 6: for(i=0;i<=5;i++)
 				{	gotoxy(4,12+(2*i));
 					if(i==5) textcolor(11);
 					else     textcolor(1);
-					cprintf("Û");
+					cprintf("Ã›");
 				} break;
 		}
        }
